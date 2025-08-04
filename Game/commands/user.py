8414f78225ca05b7b45a9ef22921067c44755ed9
Sikander <139198327+Yash-coder53@users.noreply.github.com>
2025-08-04 16:07:50 +0530

    Update user.py

--- Game/commands/user.py
+++ Game/commands/user.py
@@ -1,6 +1,6 @@
 from telegram import Update
 from telegram.ext import ContextTypes
-from dr_driving_bot.static import messages as msg
+from Game.static import messages as msg
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(msg.HELP_MESSAGE)
