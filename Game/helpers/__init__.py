22676c2463842e7ff65e2bc01954e093f30ebbbf
Sikander <139198327+Yash-coder53@users.noreply.github.com>
2025-08-04 15:43:52 +0530

    Update __init__.py

--- Game/helpers/__init__.py
+++ Game/helpers/__init__.py
@@ -1,1 +1,31 @@
+"""
+Helpers Module - Utilities and Decorators
 
+Provides:
+- Access control decorators
+- Game utility functions
+- Common helper methods
+"""
+
+from .decorators import (
+    restricted,
+    check_ban
+)
+
+from .utils import (
+    format_score,
+    generate_road,
+    check_collision
+)
+
+__all__ = [
+    'restricted',
+    'check_ban',
+    'format_score',
+    'generate_road',
+    'check_collision'
+]
+
+# Constants for helper functions
+MAX_SPEED = 3
+BASE_SCORE_INCREMENT = 10
